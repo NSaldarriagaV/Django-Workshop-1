2540a63d54478c0bacf24f55040118b45b03bef4
NSaldarriagaV <141058207+NSaldarriagaV@users.noreply.github.com>
2025-02-10 09:49:02 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Nicolas'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
+
+def Onem(request):
+    return HttpResponse('<h1>ESTEBITAN</h1>')
